983184d87d491e527230ba9daada25731b667780
GustsG <74991189+GustsG@users.noreply.github.com>
2024-06-18 00:54:26 +0300

    aaa

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+function switchSection() {
+  var section1 = document.getElementById('pirmais');
+  var section2 = document.getElementById('pirmaisdivi');
+
+  if (section1.style.display === 'none') {
+    section1.style.display = 'block';
+    section2.style.display = 'none';
+  } else {
+    section1.style.display = 'none';
+    section2.style.display = 'block';
+  }
+}
+
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
@@ -7,3 +20,4 @@ window.addEventListener("scroll", () => {
       toTop.classList.remove("active");
     }
   })
+
